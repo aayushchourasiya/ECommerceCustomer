f263301e465f4dc9861378e685beaacc80cf4adc
Dr.Mac <dr.mac@DrMacs-iMac.local>
2022-01-27 19:51:13 +0530

    Many Changes

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,49 @@
-import { View, Text } from 'react-native';
+import {View, Text, FlatList} from 'react-native';
 import React from 'react';
-import { MainStyles } from '../../assets/styles';
+import {MainStyles} from '../../assets/styles';
+import {ButtonLarge} from '../Reusable/';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 export const Home = () => {
+  const categories = ['Technology', 'Kitchen', 'Furniture'];
   return (
     <View style={MainStyles.mainBackground}>
-      <Text></Text>
+      <View>
+        <Text style={MainStyles.textLarge}>Categories</Text>
+        <FlatList
+          horizontal
+          data={categories}
+          style={{marginTop:20}}
+          renderItem={({item, index}) => {
+            return (
+              <TouchableOpacity
+                style={[
+                  MainStyles.buttonLarge,
+                  {
+                    marginHorizontal: 20,
+                    width: 300,
+                    height: 150,
+                    justifyContent: 'center',
+                  },
+                ]}>
+                <Text style={MainStyles.textLarge}>{item}</Text>
+              </TouchableOpacity>
+              // <ButtonLarge
+              //   style={{
+              //     alignSelf: 'center',
+              //     justifyContent: 'center',
+              //     marginTop: 10,
+              //     height: 140,
+              //     width: '95%',
+              //   }}
+              //   text={item}
+              //   onPress={() => alert('abc')}
+              //   key={index}
+              // />
+            );
+          }}
+        />
+      </View>
     </View>
   );
 };
--- src/Components/Reusable/ButtonLarge.js
+++ src/Components/Reusable/ButtonLarge.js
@@ -2,12 +2,13 @@ import React from 'react';
 import {Text, TouchableOpacity} from 'react-native';
 import {fontFamily} from '../../assets/constants';
 import {MainStyles} from '../../assets/styles';
-export const ButtonLarge = ({text, onPress, disabled}) => {
+export const ButtonLarge = ({text, onPress, disabled, style}) => {
   return (
     <TouchableOpacity
       style={[
         disabled ? MainStyles.buttonLargeDisabled : MainStyles.buttonLarge,
         {width: '80%'},
+        style,
       ]}
       onPress={() => onPress()}
       disabled={disabled || false}>
